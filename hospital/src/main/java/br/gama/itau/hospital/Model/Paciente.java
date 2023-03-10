6fac0a173fcfa6bc13c0a27afb13ed40b69a5127
alonsocamilla1 <alocamilla@gmail.com>
2023-03-10 11:38:13 -0300

    commit inicial

---
+++ hospital/src/main/java/br/gama/itau/hospital/Model/Paciente.java
@@ -0,0 +1,13 @@
+package br.gama.itau.hospital.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Paciente {
+    private String nome;
+    private String CPF;
+    private String telefone;
+    
+}
---
+++ hospital/src/main/java/br/gama/itau/hospital/Service/PacienteService.java
@@ -0,0 +1,12 @@
+package br.gama.itau.hospital.Service;
+
+import br.gama.itau.hospital.Model.Paciente;
+
+public class PacienteService {
+
+    //public Paciente getById(String CPF) {
+
+        
+    }
+    
+
